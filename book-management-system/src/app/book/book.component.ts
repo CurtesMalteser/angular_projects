1289b440176422c5183a215c2e25076653cad29a
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-25 10:04:53 +0100

    Fixes compilation issues
    * adds app.module.ts
    * fixes main.ts by importing AppModule
    * deletes standalone from components

--- book-management-system/src/app/book/book.component.ts
+++ book-management-system/src/app/book/book.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book',
-  standalone: true,
-  imports: [],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
