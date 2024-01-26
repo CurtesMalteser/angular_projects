05a6625e76f76833e443d433b7da137410e9ca4e
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-26 13:51:55 +0100

    ReservationListComponent gets all reservations from ReservationService correctly

--- hotel-app/src/app/reservation-list/reservation-list.component.ts
+++ hotel-app/src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { RESERVATION_SERVICE, ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit{
+
+  private reservations: Reservation[] = []
+
+  constructor(
+    @Inject(RESERVATION_SERVICE) private reservationService: ReservationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations()
+    console.log(this.reservations)
+  }
 
 }
