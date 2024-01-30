ad9af62a50c88762fc38a4e585f71315506b0326
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-30 19:43:22 +0100

    Implements responsive grid layout by using Flex-Layout

--- animal-paintings-app/src/app/product/product.module.ts
+++ animal-paintings-app/src/app/product/product.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { PRODUCT_SERVICE, ProductServiceImpl } from './product.service';
 import { MatCardModule } from '@angular/material/card';
+import { FlexModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     MatCardModule,
+    FlexModule,
   ],
   providers: [{ provide: PRODUCT_SERVICE, useClass: ProductServiceImpl }]
 })
