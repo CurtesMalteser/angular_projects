f86411d20734c6ef8b301c2c4779402aef9fc473
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-01-30 23:09:34 +0100

    Implements total price

--- animal-paintings-app/src/app/cart/cart-view/cart-view.component.ts
+++ animal-paintings-app/src/app/cart/cart-view/cart-view.component.ts
@@ -9,18 +9,27 @@ import { Product } from '../../models/product';
 })
 export class CartViewComponent implements OnInit {
 
-  cartItems : Product[] = []
+  cartItems: Product[] = []
+  totalPrice: number = 0
 
   constructor(
     @Inject(CART_SERVICE) private cartService: CartService,
-    ) {}
+  ) { }
 
-    ngOnInit(): void {
-      this.cartService.getCartItems()
+  ngOnInit(): void {
+    this.cartService.getCartItems()
       .subscribe({
-        next: (items) =>  this.cartItems = items,
+        next: (items) => {
+          this.cartItems = items
+          this.totalPrice = this.getTotalPrice()
+        },
         error: (err) => console.error(err)
       })
-    }
-
   }
+
+  private getTotalPrice(): number {
+    return this.cartItems
+      .map(item => item.price)
+      .reduce((sum, current) =>  Number(sum) + Number(current), 0)
+  }
+}
