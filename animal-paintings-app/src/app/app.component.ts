3cd0d7f2152807bf602c790ea21aad0b64e08f42
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-02 14:07:16 +0100

    Tests AppComponent

--- animal-paintings-app/src/app/app.component.ts
+++ animal-paintings-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'animal-paintings-app';
+  title = 'Amazing Animal Paintings'
+  description = 'Make your home special with beautiful and unique artwork.'
 }
