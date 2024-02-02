37111884e85ce43808be0529c0e76a9d844e9e45
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-02 15:45:47 +0100

    Fixes services tests

--- animal-paintings-app/src/app/cart/cart.service.spec.ts
+++ animal-paintings-app/src/app/cart/cart.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CartService, CartServiceImpl } from './cart.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CartService', () => {
   let service: CartService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
     service = TestBed.inject(CartServiceImpl);
   });
 
