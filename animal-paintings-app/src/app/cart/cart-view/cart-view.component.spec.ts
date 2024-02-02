670c1bd23b5988c6b0ac5d6cf6db07a48f668e1d
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-02 14:41:35 +0100

    Fixes CartViewComponent test

--- animal-paintings-app/src/app/cart/cart-view/cart-view.component.spec.ts
+++ animal-paintings-app/src/app/cart/cart-view/cart-view.component.spec.ts
@@ -1,20 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartViewComponent } from './cart-view.component';
+import { CART_SERVICE, CartService } from '../cart.service';
+import { Observable, of } from 'rxjs';
+import { Product } from '../../models/product';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Injectable } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
+@Injectable()
+class MockCartService implements CartService {
+  addToCart(product: Product): Observable<Product> {
+    return of(product)
+  }
+  getCartItems(): Observable<Product[]> {
+    return of([new Product(1, "test", 1, "")])
+  }
+  clearCart(): Observable<void> {
+    return of()
+  }
+  checkout(product: Product[]): Observable<void> {
+    return of()
+    //throw new Error('Method not implemented.');
+  }
+}
+
+const SIGNALR_PROVIDER = [ { provide: CART_SERVICE, useClass: MockCartService } ];
 
 describe('CartViewComponent', () => {
   let component: CartViewComponent;
   let fixture: ComponentFixture<CartViewComponent>;
+  let snackbar: MatSnackBar;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CartViewComponent]
-    })
-    .compileComponents();
-    
+      declarations: [CartViewComponent],
+      imports: [
+        MatCardModule,
+        MatListModule,
+      ],
+      providers: [
+        SIGNALR_PROVIDER
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CartViewComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    snackbar = TestBed.inject(MatSnackBar)
   });
 
   it('should create', () => {
